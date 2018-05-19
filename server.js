11915668d94e1078f8c40d2eb9c19bec214c8e86
Mohammad Inzamam Malik <malikasinger1@gmail.com>
2018-05-19 14:48:31 +0500

    chat app done

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ var s = new server({ port: 3001 })
 // s.once
 s.on("connection", (ws) => {
     console.log("i got connected a new client");
-    // console.log("all connected clients array: ", s.clients)
+    console.log("all connected clients array: ", s.clients)
 
     // // to send message to all connected clients
-    // s.clients.map(eachClient=>{
+    // s.clients.forEach(eachClient=>{
     //     eachClient.send("some message" + actualMessage)
     // })
 
@@ -17,7 +17,28 @@ s.on("connection", (ws) => {
     // any message sent by the client will receive here
     ws.on('message', actualMessage => {
         console.log("received: ", actualMessage);
-        ws.send("reply from server")
+        let message = JSON.parse(actualMessage)
+
+        if (message.type == "enroll") {
+
+            //adding another attribute in that client instance
+            ws.clientName = message.name
+            return;
+
+        } else if (message.type == "message") {
+
+            s.clients.forEach(eachClient => {
+                if(eachClient != ws) // send to all but do not send to the sender again
+                    eachClient.send( JSON.stringify({
+                        type: "message",
+                        text: message.text,
+                        from: ws.clientName
+                    }))
+            })
+
+        }
+
+
     })
 
     //when a browser window close, or somehow a client is disconnect
